bbddec91730e14388d32579186ec21d515aa86cd
5Otien <75800465+5Otien@users.noreply.github.com>
2025-11-27 18:42:49 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 """CloudSecOps Scanner - Multi-cloud security scanning tool"""
 
 __version__ = '1.0.0'
-__author__ = 'Bilal Mostefi'
+__author__ = '5Otien'
 
 from .scanner import CloudSecOpsScanner, ScanResults
 from .config import Config, load_config
